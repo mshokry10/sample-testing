1ebd442b8fc8e8c9745ce93eb1a54020485dc4cb
Mohamed Ragab <mohamed.ragab@orange.com>
2014-07-09 14:43:44 +0200

    Basic Code with Tests

---
+++ src/main/java/com/appscorner/samples/Math.java
@@ -0,0 +1,23 @@
+package com.appscorner.samples;
+
+import java.math.BigInteger;
+
+public class Math {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public double div(double a, double b) {
+        if (b == 0d) throw new IllegalArgumentException();
+        return a / b;
+    }
+
+    public BigInteger slowFactorial(int num) {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {}
+        if (num == 0) return BigInteger.ONE;
+        return BigInteger.valueOf(num).multiply(slowFactorial(num - 1));
+    }
+}
---
+++ src/test/java/com/appscorner/samples/MathTest.java
@@ -0,0 +1,34 @@
+package com.appscorner.samples;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+public class MathTest {
+
+    @Test
+    public void testAdd() throws Exception {
+        final Math math = new Math();
+        assertEquals("Addition", 5, math.add(2, 3));
+    }
+
+    @Test
+    public void testAddWithHamcrestMatchers() throws Exception {
+        final Math math = new Math();
+        assertThat("Addition", math.add(2, 3), is(5));
+    }
+
+    @Test
+    public void testDiv() throws Exception {
+        final Math math = new Math();
+        assertThat("Division", math.div(3, 2), is(1.5));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDivByZero() throws Exception {
+        final Math math = new Math();
+        math.div(1, 0);
+    }
+}
